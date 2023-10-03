4fe9ed91b57a76181e00e41fbdd607cf3d241663
mehmet-mhy <mehmet.mhy@gmail.com>
2023-10-03 15:59:57 -0600

    chatgpt...

--- scrapper/print_all.py
+++ scrapper/print_all.py
@@ -1,43 +1,26 @@
 import json
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import sessionmaker
 
 # Note: Adjust the import statement based on your file and class structure
 from main import Post, Comment, initialize_db  
 
+def object_as_dict(obj):
+    return {column.key: getattr(obj, column.key) for column in inspect(obj).mapper.column_attrs}
+
 def retrieve_and_print_data(session):
     # Retrieve all posts from the database
     posts = session.query(Post).all()
 
-    # Retrieve all comments for each post
     data_list = []
     for post in posts:
-        post_data = {
-            "id": post.id,
-            "subreddit": post.subreddit,
-            "title": post.title,
-            "selftext": post.selftext,
-            "upvote_ratio": post.upvote_ratio,
-            "ups": post.ups,
-            "downs": post.downs,
-            "score": post.score,
-            "permalink": post.permalink,
-            "recorded": post.recorded,
-            "comments": []
-        }
+        post_data = object_as_dict(post)
+        post_data["comments"] = []
 
         # Retrieve comments related to the post
         comments = session.query(Comment).filter_by(post_id=post.id).all()
         for comment in comments:
-            comment_data = {
-                "author": comment.author,
-                "comment": comment.comment,
-                "url": comment.url,
-                "date": comment.date,
-                "score": comment.score,
-                "recorded": comment.recorded,
-                "reddit_id": comment.reddit_id  # include reddit_id in data
-            }
+            comment_data = object_as_dict(comment)
             post_data["comments"].append(comment_data)
 
         data_list.append(post_data)
@@ -47,7 +30,7 @@ def retrieve_and_print_data(session):
     print(json_str)
 
 # Initialize DB session
-session = initialize_db()  # Utilize the function from your main script
+session = initialize_db()
 
 # Call the function to retrieve and print the data
 retrieve_and_print_data(session)
